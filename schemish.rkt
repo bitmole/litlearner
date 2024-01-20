d1082477d15d21a8ff15ceaaa12cad00e44ade0b
bitmole <robert@idea-loop.com>
2024-01-19 21:20:23 -0700

    a little schemish

---
+++ schemish.rkt
@@ -0,0 +1,31 @@
+#lang racket
+(require malt)
+
+(define pie 3.14)
+
+(define a-radius 8.4)
+
+(define an-area
+  (* pie 
+     (* a-radius a-radius)))
+
+(define area-of-circle
+  (lambda (r)
+    (* pie
+       (* r r))))
+
+(define area-of-rectangle
+  (lambda (width)
+    (lambda (height)
+      (* width height))))
+
+(define double-result-of-f
+  (lambda (f) 
+    (lambda (z)
+      (* 2 (f z)))))
+
+(define abs
+  (lambda (x)
+    (cond
+      ((< x 0) (- 0 x))
+      (else x))))
